5486207ebe06cf2923baaea05b1e436c8e4f3a63
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-03-29 17:58:26 +0530

    optimze the solution

--- two_pointer/mergeStringAlt.cpp
+++ two_pointer/mergeStringAlt.cpp
@@ -6,8 +6,12 @@ public:
     string mergeAlternately(string word1, string word2) {
         int N = word1.size(), M = word2.size();
         string StoringList;
+
+        // two pointer for the merge the string alternately and store in the StoringList variable
         int i = 0, j = 0;
 
+            // lentgh of the loop is depends on the length of the string and we will continue the loop until both pointer reaches the end of their respective strings
+            
         while (i < N || j < M) {
             if (i < N) StoringList += word1[i++];
             if (j < M) StoringList += word2[j++];
