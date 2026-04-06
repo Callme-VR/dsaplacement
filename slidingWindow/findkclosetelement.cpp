bad16f2c1a9247aeb5801edc3871fe3c784911c3
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-04-06 15:05:02 +0530

    one codeforces question with 800 rating and thre33 slinding window questions

---
+++ slidingWindow/findkclosetelement.cpp
@@ -0,0 +1,29 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> findClosestElements(vector<int> &arr, int k, int x)
+    {
+        int start = 0;
+        int end = arr.size() - k;
+
+        while (start < end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (x - arr[mid] > arr[mid + k] - x)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid;
+            }
+        }
+        return vector<int>(arr.begin() + start, arr.begin() + start + k);
+    }
+};
