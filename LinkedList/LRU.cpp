1d781a6b66752763711f147d5d56f2267288321f
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-04-25 16:48:40 +0530

    add LRUCache implementation with get and put methods

---
+++ LinkedList/LRU.cpp
@@ -0,0 +1,58 @@
+#include <list>
+#include <unordered_map>
+using namespace std;
+
+class LRUCache {
+private:
+    int cap;
+
+    // Doubly linked list: stores {key, value}
+    list<pair<int, int>> cacheList;
+
+    // Map: key -> iterator of list
+    unordered_map<int, list<pair<int, int>>::iterator> cacheMap;
+
+public:
+    // Constructor
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
+
+    int get(int key) {
+        // Key not found
+        if (cacheMap.find(key) == cacheMap.end()) {
+            return -1;
+        }
+
+        // Move accessed node to front (most recently used)
+        cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+
+        return cacheMap[key]->second;
+    }
+
+    void put(int key, int value) {
+        // If key already exists
+        if (cacheMap.find(key) != cacheMap.end()) {
+            // Move to front
+            cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+
+            // Update value
+            cacheMap[key]->second = value;
+            return;
+        }
+
+        // If capacity full → remove least recently used (back)
+        if (cacheList.size() == cap) {
+            int lruKey = cacheList.back().first;
+
+            cacheList.pop_back();        // remove from list
+            cacheMap.erase(lruKey);      // remove from map
+        }
+
+        // Insert new element at front
+        cacheList.push_front({key, value});
+
+        // Store iterator in map
+        cacheMap[key] = cacheList.begin();
+    }
+};
